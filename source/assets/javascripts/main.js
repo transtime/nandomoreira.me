1d98d027414d7ba4dd55a57f8302c7d4a6e00e78
Fernando Moreira <nandomoreira.me@gmail.com>
2016-06-12 17:33:16 -0300

    Add text parallax on header and change primary color

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,9 +1,29 @@
 //= require vendor/jquery.min.js
 
 (function($) {
+  'use strict';
+
   $(document).on('click', '[data-toggle="nav"], .nav-opened .site-main', function(e) {
     e.preventDefault();
     $('body').toggleClass('nav-opened');
   });
-})(jQuery);
 
+  var titleParallax = {
+    textParallax : document.querySelector('.text-parallax'),
+
+    init: function() {
+      window.addEventListener("scroll", function(event) {
+        titleParallax.update(this.scrollY);
+      }, false);
+    },
+    update: function(scroll) {
+      this.textParallax.style['-webkit-transform'] = 'translateY('+ (scroll * .5) +'px)';
+      this.textParallax.style['-moz-transform'] = 'translateY('+ (scroll * .5) +'px)';
+      this.textParallax.style['transform'] = 'translateY('+ (scroll * .5) +'px)';
+      this.textParallax.style['opacity'] = (1 - ( scroll * .005 ));
+    }
+  };
+
+  titleParallax.init();
+
+})(jQuery);
