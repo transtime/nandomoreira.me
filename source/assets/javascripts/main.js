c2523810e4c6df15946323bb610895ddb910c71f
Fernando Moreira <nandomoreira.me@gmail.com>
2016-06-17 10:30:05 -0300

    Create a _site.js and remove rotjar test

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,31 +1,5 @@
-//= require vendor/particles.min.js
-//= require vendor/jquery.min.js
-//= require particles.js
+//= require vendor/jquery.min
+//= require _site
 
-(function($) {
-  'use strict';
-
-  $(document).on('click', '[data-toggle="nav"], .nav-opened .site-main', function(e) {
-    e.preventDefault();
-    $('body').toggleClass('nav-opened');
-  });
-
-  var titleParallax = {
-    textParallax : document.querySelector('.text-parallax'),
-
-    init: function() {
-      window.addEventListener("scroll", function(event) {
-        titleParallax.update(this.scrollY);
-      }, false);
-    },
-    update: function(scroll) {
-      this.textParallax.style['-webkit-transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['-moz-transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['opacity'] = (1 - ( scroll * .005 ));
-    }
-  };
-
-  titleParallax.init();
-
-})(jQuery);
+//= require vendor/particles.min
+//= require particles
